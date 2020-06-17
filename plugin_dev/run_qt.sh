bb540a8fe36e9e573ee4f51f055de8b022485359
Ciaran Evans <9111975+ciaranevans@users.noreply.github.com>
2020-06-17 14:19:03 +0100

    Point scripts to plugin_dev

--- plugin_dev/run_qt.sh
+++ plugin_dev/run_qt.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 xhost +
-docker build -t qgis-development:latest -f qgis_development/Dockerfile .
+docker build -t qgis-development:latest -f plugin_dev/Dockerfile .
 docker run -it --rm --privileged --net=host --env="DISPLAY" -v $(pwd):/qgis-workspace \
 qgis-development:latest qgis
